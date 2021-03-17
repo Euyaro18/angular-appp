739929eb88af13031cae9ec9ace73ee45262d973
euyaro18 <eugene.yaro@ibm.com>
2021-03-17 16:16:13 +0100

    commit

---
+++ client/src/app/dist/connection.dev.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "CSI_lab_contacts"
+});
+con.connect(function (err) {
+  if (err) throw err;
+  console.log("Connecté à la base de données MySQL!");
+});
